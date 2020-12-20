80c30a6b7bdbd64704427958db91318533f19a8c
hnguyen28git <hdzhdz@uw.edu>
2020-12-20 12:36:14 -0800

    Updates

---
+++ precache-manifest.e41bb0fb8e12fd84c6b51c4775b62ea4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b866b6fa9dca77a05254aa37e6ef190",
+    "url": "/tmpweb/index.html"
+  },
+  {
+    "revision": "af8f7f2dd56ac71ac1f8",
+    "url": "/tmpweb/static/css/2.9522b9fe.chunk.css"
+  },
+  {
+    "revision": "a6fba58b341b39704a6e",
+    "url": "/tmpweb/static/css/main.ab2501cf.chunk.css"
+  },
+  {
+    "revision": "af8f7f2dd56ac71ac1f8",
+    "url": "/tmpweb/static/js/2.edb5f950.chunk.js"
+  },
+  {
+    "revision": "e465a1d90cd90ed95cee5d0fa63a8915",
+    "url": "/tmpweb/static/js/2.edb5f950.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6fba58b341b39704a6e",
+    "url": "/tmpweb/static/js/main.d3bd58c1.chunk.js"
+  },
+  {
+    "revision": "51e59a53f1275e6683c9",
+    "url": "/tmpweb/static/js/runtime-main.d1d0cb6e.js"
+  },
+  {
+    "revision": "27a47438816b8d2dce1e61188cb2c487",
+    "url": "/tmpweb/static/media/video-1.27a47438.mp4"
+  }
+]);
